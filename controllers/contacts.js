38bdee1d7b45e5e27bd6ead2fdb292c3a1e49960
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2023-10-31 23:44:44 +0200

    add ctrl and decorator

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const contacts = require("../models/contacts");
-const { HttpError } = require("../helpers");
+const { HttpError, ctrlWrapper } = require("../helpers");
 
 const Joi = require("joi");
-
-const router = express.Router();
 
 const addShema = Joi.object({
   name: Joi.string().required(),
@@ -12,84 +10,64 @@ const addShema = Joi.object({
   phone: Joi.string().required(),
 });
 
-const listContacts = async (req, res, next) => {
-  try {
-    const result = await contacts.listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+const listContacts = async (req, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
 };
 
-const getContactById = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.getContactById(contactId);
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
 
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-
-    res.json(result);
-  } catch (error) {
-    next(error);
+  if (!result) {
+    throw HttpError(404, "Not found");
   }
+
+  res.json(result);
 };
 
-const addContact = async (req, res, next) => {
-  try {
-    const { error } = addShema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-
-    const result = await contacts.addContact(req.body);
-
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
+const addContact = async (req, res) => {
+  const { error } = addShema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
   }
+
+  const result = await contacts.addContact(req.body);
+
+  res.status(200).json(result);
 };
 
-const removeContact = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.removeContact(contactId);
+const removeContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
 
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-
-    res.json({ message: "contact deleted" });
-  } catch (error) {
-    next(error);
+  if (!result) {
+    throw HttpError(404, "Not found");
   }
+
+  res.json({ message: "contact deleted" });
 };
 
-const updateContact = async (req, res, next) => {
-  try {
-    const { error } = addShema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-
-    const { contactId } = req.params;
-    const result = await contacts.updateContact(contactId, req.body);
-
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-
-    res.json(result);
-  } catch (error) {
-    next(error);
+const updateContact = async (req, res) => {
+  const { error } = addShema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
   }
+
+  const { contactId } = req.params;
+  const result = await contacts.updateContact(contactId, req.body);
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.json(result);
 };
 
 module.exports = {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
+  listContacts: ctrlWrapper(listContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  updateContact: ctrlWrapper(updateContact),
 };
